d41dc4b32fc209ea4fa87661d4fc5a640970d162
brendonstahl97 <brendonstahl97@gmail.com>
2022-02-08 14:30:56 -0500

    Landing pane 1 established

--- pb-and-jj/src/pages/landing/landing.js
+++ pb-and-jj/src/pages/landing/landing.js
@@ -7,16 +7,19 @@ import './landing.scss';
 function Landing() {
     return (
         <div className="landing-page">
-            <img className="human1" src='./img/human.png' alt='everyday human' />
-            <img className="menacing1" src='./img/menacing.png' alt="menacing kanji"></img>
-            <img className="menacing2" src='./img/menacing.png' alt="menacing kanji"></img>
-            <img className="menacing3" src='./img/menacing.png' alt="menacing kanji"></img>
-            <div className="text">
-                <h1>Parker, Brendon, and JoJo</h1>
-                <h2><b>The</b> JoJo's Bizarre Adventure Watch/Re-watch Podcast</h2>
-                <Button label='Learn More'></Button>
-            </div>
+            <div className="pane1">
 
+                <img className="human1" src='./img/human.png' alt='everyday human' />
+                <img className="menacing1" src='./img/menacing.png' alt="menacing kanji"></img>
+                <img className="menacing2" src='./img/menacing.png' alt="menacing kanji"></img>
+                <img className="menacing3" src='./img/menacing.png' alt="menacing kanji"></img>
+                <div className="text">
+                    <h1 className='appear-up-0' >Parker, Brendon, and JoJo</h1>
+                    <h2 className='appear-up-1' ><b>The</b> JoJo's Bizarre Adventure Watch/Re-watch Podcast</h2>
+                    <Button label='Learn More'></Button>
+                </div>
+
+            </div>
         </div>
     );
 }
