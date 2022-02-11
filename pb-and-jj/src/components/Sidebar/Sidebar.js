a066a09f4a556a88b1285bae6adbcb6a1ba0a596
brendonstahl97 <brendonstahl97@gmail.com>
2022-02-10 22:43:44 -0500

    sidebar tweaks

--- pb-and-jj/src/components/Sidebar/Sidebar.js
+++ pb-and-jj/src/components/Sidebar/Sidebar.js
@@ -6,19 +6,24 @@ function Sidebar(props) {
 
     const [open, setOpen] = useState(false);
 
+    const handleMobileLinkClick = () => {
+        if (!open) { return; };
+        setOpen(false);
+    };
+
     return (
         <section className='sidebar'>
             <nav className={open ? 'mobile-open' : ''}>
-                <Link className='logo navLink' to='/'>PB&JJ</Link>
+                <Link className='logo navLink' to='/' onClick={() => !open ? '' : setOpen(!open)}>PB&JJ</Link>
                 <ul>
                     <li>
-                        <Link className='navLink' to='/about'>About Us</Link>
+                        <Link className='navLink' to='/about' onClick={() => !open ? '' : setOpen(!open)}>About Us</Link>
                     </li>
                     <li>
-                        <Link className='navLink' to='/ranked-lists'>Episode Rankings</Link>
+                        <Link className='navLink' to='/ranked-lists' onClick={() => !open ? '' : setOpen(!open)}>Episode Rankings</Link>
                     </li>
                     <li>
-                        <Link className='navLink' to='/contact'>Contact Us</Link>
+                        <Link className='navLink' to='/contact' onClick={() => !open ? '' : setOpen(!open)}>Contact Us</Link>
                     </li>
                 </ul>
                 <div className='mobile-close-btn mobile-btn'>
