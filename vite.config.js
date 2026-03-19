c98ac6d7209f1e52e47b0ffed33fe55eb831c380
brendonstahl97 <brendonstahl97@gmail.com>
2026-03-19 18:19:29 -0400

    updated base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://brendonstahl97.github.io/test-4299/",
+  base: "/",
   plugins: [react()],
 })
